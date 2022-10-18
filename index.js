dfe5511f3ed35d0e6f0a893b892df37db3af77b6
mydatahack <thon6thon6@hotmail.com>
2022-10-18 16:56:41 +1100

    fix(index): update import in index file

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   rules: {
-    'ban-raw-apollo-use-subscription': require('./eslint-rules/BanRawApolloUseSubscription.js'),
-    'ban-parse-float': require('./eslint-rules/BanParseFloat.js'),
-    'force-typed-api-requests': require('./eslint-rules/ForceTypedApiRequests.js'),
-    'styled-components-naming': require('./eslint-rules/StyledComponentsNaming.js'),
-    'prevent-unused-imports': require('./eslint-rules/PreventUnusedImports.js'),
+    'ban-raw-apollo-use-subscription': require('./eslint-rules/BanRawApolloUseSubscription'),
+    'ban-parse-float': require('./eslint-rules/BanParseFloat'),
+    'force-typed-api-requests': require('./eslint-rules/ForceTypedApiRequests'),
+    'styled-components-naming': require('./eslint-rules/StyledComponentsNaming'),
+    'prevent-unused-imports': require('./eslint-rules/PreventUnusedImports'),
   },
 }
